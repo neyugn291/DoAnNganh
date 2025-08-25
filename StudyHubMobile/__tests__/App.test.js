a567904fba6648b3aa644afac63bf00b9de11bf9
neyugn291 <dhnguyen243@gmail.com>
2025-08-26 01:59:44 +0700

    remove jest

--- StudyHubMobile/__tests__/App.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react-native';
-import App from '../App';
-
-test('renders correctly', () => {
-  const { getByText } = render(<App />);
-  expect(getByText(/Test/i)).toBeTruthy();
-});
