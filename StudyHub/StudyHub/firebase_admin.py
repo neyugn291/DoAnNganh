dccabe4ac20ff9aab63b67eb570c7e6a70407292
neyugn291 <dhnguyen243@gmail.com>
2025-09-06 21:28:29 +0700

    some fix

--- StudyHub/StudyHub/firebase_admin.py
+++ StudyHub/StudyHub/firebase_admin.py
@@ -1,7 +1,19 @@
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials, messaging
 
-cred = credentials.Certificate("StudyHub/studyhub-75cef-firebase-adminsdk-fbsvc-6b802fa5f5.json")
+# Lấy JSON từ biến môi trường
+service_account_info = os.environ["FIREBASE_ADMIN_JSON"]
+
+# Chuyển chuỗi JSON thành dict
+service_account_info = json.loads(service_account_info)
+
+# Fix private_key: thay "\\n" thành "\n"
+service_account_info["private_key"] = service_account_info["private_key"].replace("\\n", "\n")
+
+# Initialize Firebase Admin
+cred = credentials.Certificate(service_account_info)
 firebase_admin.initialize_app(cred)
 
 def send_push_notification(token, title, body):
@@ -11,4 +23,3 @@ def send_push_notification(token, title, body):
     )
     response = messaging.send(message)
     return response
-
