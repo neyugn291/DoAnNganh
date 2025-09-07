d7953069800275824cb65cf622e0e610f18e5d64
neyugn291 <dhnguyen243@gmail.com>
2025-09-07 18:08:50 +0700

    Add Procfile, requirements.txt, railway.toml for deploy

--- StudyHubMobile/configs/Apis.js
+++ StudyHubMobile/configs/Apis.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://192.168.1.15:8000/api";
+const BASE_URL = "http://192.168.120.39:8000/api";
 
 export const endpoints = {
     'categories': '/courses/categories/',
